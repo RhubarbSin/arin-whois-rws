b00e6ed94b08683c92aef543dba99d53ce116671
Rhubarb Sin <rhubarbsin@gmail.com>
2013-08-31 18:56:40 -0400

    implement class for resource args

--- parse.py
+++ parse.py
@@ -1,24 +1,62 @@
 import argparse
 
+class ParserResource(object):
+
+    def __init__(self, command, positional=None, *options):
+        self.command = command
+        self.positional = positional
+        self.option = dict((option[0], option[1]) for option in options)
+
+    @property
+    def help_(self):
+        x = self.positional[0].upper() if self.positional else ''
+        for name in self.option:
+            x += (' --%s %s' % (name, name.upper()))
+        return x
+
 class Parser(argparse.ArgumentParser):
+
+    resources = (ParserResource('poc', ('handle', 'POC Handle')),
+                 ParserResource('org', ('handle', 'Org handle')),
+                 ParserResource('net', ('handle', 'Net handle')),
+                 ParserResource('asn', ('handle', 'ASN handle')),
+                 ParserResource('customer', ('handle', 'Customer handle')),
+                 ParserResource('rdns', ('name', 'Delegation name')),
+
+                 ParserResource('orgs', None,
+                                ('handle', 'Org handle'),
+                                ('name', 'Org Name'),
+                                ('dba', 'Org DBA')))
 
     def populate(self):
         self.add_argument('--output', choices=('xml', 'text', 'html'),
                           default='text')
         subparsers = self.add_subparsers(title='Commands', metavar='',
                                          dest='call')
-        self._add_org(subparsers)
-        self._add_orgs(subparsers)
+        for resource in self.resources:
+            self._add_subparser(subparsers, resource)
 
     def run(self):
         return self.parse_args()
 
-    def _add_org(self, subparsers):
-        self.org = subparsers.add_parser('org', help='HANDLE')
-        self.org.add_argument('handle', metavar='HANDLE', help='Org handle')
+    def _add_subparser(self, subparsers, resource):
+        subparser = subparsers.add_parser(resource.command,
+                                          help=resource.help_)
+        if resource.positional:
+            subparser.add_argument(resource.positional[0],
+                                   metavar=resource.positional[0].upper(),
+                                   help=resource.positional[1])
+        for name, description in resource.option.iteritems():
+            subparser.add_argument(name, metavar=name.upper(),
+                                   help=description)
 
     def _add_orgs(self, subparsers):
-        self.org = subparsers.add_parser('orgs', help='--handle HANDLE --name NAME --dba DBA')
-        self.org.add_argument('--handle', help='Org handle')
-        self.org.add_argument('--name', help='Org name')
-        self.org.add_argument('--dba', help='Org DBA')
+        subparser = subparsers.add_parser('orgs', help='--handle HANDLE --name NAME --dba DBA')
+        subparser.add_argument('--handle', help='Org handle')
+        subparser.add_argument('--name', help='Org name')
+        subparser.add_argument('--dba', help='Org DBA')
+
+    def _add_org_related(self, subparsers):
+        subparser = subparsers.add_parser('org-related', help='--handle HANDLE --resource RESOURCE')
+        subparser.add_argument('--handle', help='Org handle')
+        subparser.add_argument('--resource', help='Resource')
--- whois.py
+++ whois.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from StringIO import StringIO
+
 import parse
 import call
 
@@ -7,4 +9,16 @@ parser = parse.Parser()
 parser.populate()
 args = parser.run()
 caller = call.Caller(args)
-output = caller.run()
+payload = caller.run()
+
+stringio = StringIO()
+payload.export(stringio, 0, namespace_='')
+xml = stringio.getvalue()
+stringio.close()
+
+import lxml.etree as ET
+dom = ET.fromstring(xml)
+xslt = ET.parse('website.xsl')
+transform = ET.XSLT(xslt)
+newdom = transform(dom)
+print(ET.tostring(newdom, pretty_print=True))
