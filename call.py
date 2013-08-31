15124f8ceccdf07cefa07890aafed4e187b2a9c0
Rhubarb Sin <rhubarbsin@gmail.com>
2013-08-31 15:30:56 -0400

    return payload from call

--- call.py
+++ call.py
@@ -1,11 +1,16 @@
 import requests
 
+import payload
+from payload import org
+
 BASE_URL = 'http://whois.arin.net/rest/'
 
 class Caller(object):
 
     def __init__(self, args):
-        if args.call == 'orgs':
+        if args.call == 'org':
+            call = Org
+        elif args.call == 'orgs':
             call = Orgs
         self.call = call(**vars(args))
 
@@ -19,9 +24,6 @@ class Call(object):
 
     def __init__(self, **kwargs):
         self.url = BASE_URL + self.resource
-        for param in self.params:
-            if kwargs[param]:
-                self.url += ';%s=%s' % (param, kwargs[param])
 
     def run(self):
         print self.url
@@ -29,15 +31,48 @@ class Call(object):
         if response.status_code != requests.codes.ok:
             response.raise_for_status()
         print response.content
+        return self.module.parseString(response.content)
 
-class Org(Call):
+class SimpleCall(Call):
 
-    resource = 'org'
+    """Class for individual resources.
+
+    Calls look like /RESOURCE/HANDLE.
+    """
 
     def __init__(self, **kwargs):
-        pass
+        super(SimpleCall, self).__init__(**kwargs)
+        self.url += '/' + kwargs['handle']
+
+class UnrelatedCall(Call):
+
+
+    """Class for lists of unrelated resources.
+
+    Calls look like /RESOURCE;param=value.
+    """
+
+    def __init__(self, **kwargs):
+        super(UnrelatedCall, self).__init__(**kwargs)
+        for param in self.params:
+            if kwargs[param]:
+                self.url += ';%s=%s' % (param, kwargs[param])
+
+class RelatedCall(Call):
+
+    """Class for resources related to resources.
+
+    Calls look like /RESOURCE/HANDLE/RESOURCES.
+    """
+
+    pass
+
+class Org(SimpleCall):
+
+    resource = 'org'
+    module = payload.org
             
-class Orgs(Call):
+class Orgs(UnrelatedCall):
 
     resource = 'orgs'
     params = ('handle', 'name', 'dba')
