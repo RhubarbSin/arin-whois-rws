2d27b94bfc9b231bee3d791fc85b46847cb7b87f
Rhubarb Sin <rhubarbsin@gmail.com>
2013-09-01 13:52:19 -0400

    use tuples of tuples for all args

--- parse.py
+++ parse.py
@@ -2,7 +2,7 @@ import argparse
 
 class ParserResource(object):
 
-    def __init__(self, command, positional={}, optional={}):
+    def __init__(self, command, positional=(), optional=()):
         self.command = command
         self.positional = positional
         self.optional = optional
@@ -10,46 +10,46 @@ class ParserResource(object):
     @property
     def help_(self):
         h = ''
-        for name in self.positional:
-            h += ' %s' % name.upper()
-        for name in self.optional:
-            h += (' --%s %s' % (name, name.upper()))
+        for tup in self.positional:
+            h += ' %s' % tup[0].upper()
+        for tup in self.optional:
+            h += (' --%s %s' % (tup[0], tup[0].upper()))
         return h
 
 class Parser(argparse.ArgumentParser):
 
-    resources = (ParserResource('poc', {'handle': 'the handle of the POC'}),
-                 ParserResource('org', {'handle': 'the handle of the organization'}),
-                 ParserResource('net', {'handle': 'the handle of the network'}),
-                 ParserResource('asn', {'handle': 'the handle of the ASN'}),
-                 ParserResource('customer', {'handle': 'the handle of the customer'}),
-                 ParserResource('rdns', {'name': 'the name of the delegation (e.g. 0.192.in-addr.arpa.)'}),
+    resources = (ParserResource('poc', (('handle', 'the handle of the POC'),)),
+                 ParserResource('org', (('handle', 'the handle of the organization'),)),
+                 ParserResource('net', (('handle', 'the handle of the network'),)),
+                 ParserResource('asn', (('handle', 'the handle of the ASN'),)),
+                 ParserResource('customer', (('handle', 'the handle of the customer'),)),
+                 ParserResource('rdns', (('name', 'the name of the delegation (e.g. 0.192.in-addr.arpa.)'),)),
 
                  ParserResource('orgs',
-                                optional={'handle': 'the handle of the organization',
-                                          'name': 'the name of organization',
-                                          'dba': 'the name the organization does business as'}),
+                                optional=(('handle', 'the handle of the organization'),
+                                          ('name', 'the name of organization'),
+                                          ('dba', 'the name the organization does business as'))),
                  ParserResource('customers',
-                                optional={'handle': 'the handle of the customer',
-                                          'name': 'the name of the customer'}),
+                                optional=(('handle', 'the handle of the customer'),
+                                          ('name', 'the name of the customer'))),
                  ParserResource('pocs',
-                                optional={'handle': 'the handle of the POC',
-                                          'domain': 'the domain of the email address for the POC',
-                                          'first': 'the first name of the POC',
-                                          'last': 'the last name of the POC',
-                                          'company': 'the company name registered by the POC',
-                                          'city': 'the city registered by the POC'}),
+                                optional=(('handle', 'the handle of the POC'),
+                                          ('domain', 'the domain of the email address for the POC'),
+                                          ('first', 'the first name of the POC'),
+                                          ('last', 'the last name of the POC'),
+                                          ('company', 'the company name registered by the POC'),
+                                          ('city', 'the city registered by the POC'))),
                  ParserResource('asns',
-                                optional={'handle': 'the handle of the ASN',
-                                          'name': 'the name of the ASN'}),
+                                optional=(('handle', 'the handle of the ASN'),
+                                          ('name', 'the name of the ASN'))),
                  ParserResource('nets',
-                                optional={'handle': 'the handle of the network',
-                                          'name': 'the name of the network'}),
+                                optional=(('handle', 'the handle of the network'),
+                                          ('name', 'the name of the network'))),
                  ParserResource('rdns',
-                                optional={'name': 'the name of the delegation (e.g. 0.192.in-addr.arpa.)'}),
-                 ParserResource('pocs-related',
-                                optional={'handle': 'the handle of the POC',
-                                          'resource': 'the type of resource associated with the POC'}))
+                                optional=(('name', 'the name of the delegation (e.g. 0.192.in-addr.arpa.)'),)),
+                 ParserResource('poc-related',
+                                (('handle', 'the handle of the POC'),
+                                 ('resource', 'the type of resource associated with the POC'))))
 
     def populate(self):
         self.add_argument('--output', choices=('xml', 'text', 'html'),
@@ -65,12 +65,12 @@ class Parser(argparse.ArgumentParser):
     def _add_subparser(self, subparsers, resource):
         subparser = subparsers.add_parser(resource.command,
                                           help=resource.help_)
-        for name, description in resource.positional.iteritems():
-            subparser.add_argument(name, metavar=name.upper(),
-                                   help=description)
-        for name, description in resource.optional.iteritems():
-            subparser.add_argument('--%s' % name, metavar=name.upper(),
-                                   help=description)
+        for tup in resource.positional:
+            subparser.add_argument(tup[0], metavar=tup[0].upper(),
+                                   help=tup[1])
+        for tup in resource.optional:
+            subparser.add_argument('--%s' % tup[0], metavar=tup[0].upper(),
+                                   help=tup[1])
 
     def _add_org_related(self, subparsers):
         subparser = subparsers.add_parser('org-related', help='--handle HANDLE --resource RESOURCE')
