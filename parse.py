cf7ff7668c36d3f38eb86933949368f10907fd3d
Rhubarb Sin <rhubarbsin@gmail.com>
2013-09-01 13:16:47 -0400

    use dict for positional args

--- parse.py
+++ parse.py
@@ -2,52 +2,54 @@ import argparse
 
 class ParserResource(object):
 
-    def __init__(self, command, positional=None, *options):
+    def __init__(self, command, positional={}, optional={}):
         self.command = command
         self.positional = positional
-        self.option = dict((option[0], option[1]) for option in options)
+        self.optional = optional
 
     @property
     def help_(self):
-        h = self.positional[0].upper() if self.positional else ''
-        for name in self.option:
+        h = ''
+        for name in self.positional:
+            h += ' %s' % name.upper()
+        for name in self.optional:
             h += (' --%s %s' % (name, name.upper()))
         return h
 
 class Parser(argparse.ArgumentParser):
 
-    resources = (ParserResource('poc', ('handle', 'the handle of the POC')),
-                 ParserResource('org', ('handle', 'the handle of the organization')),
-                 ParserResource('net', ('handle', 'the handle of the network')),
-                 ParserResource('asn', ('handle', 'the handle of the ASN')),
-                 ParserResource('customer', ('handle', 'the handle of the customer')),
-                 ParserResource('rdns', ('name', 'the name of the delegation (e.g. 0.192.in-addr.arpa.)')),
+    resources = (ParserResource('poc', {'handle': 'the handle of the POC'}),
+                 ParserResource('org', {'handle': 'the handle of the organization'}),
+                 ParserResource('net', {'handle': 'the handle of the network'}),
+                 ParserResource('asn', {'handle': 'the handle of the ASN'}),
+                 ParserResource('customer', {'handle': 'the handle of the customer'}),
+                 ParserResource('rdns', {'name': 'the name of the delegation (e.g. 0.192.in-addr.arpa.)'}),
 
-                 ParserResource('orgs', None,
-                                ('handle', 'the handle of the organization'),
-                                ('name', 'the name of organization'),
-                                ('dba', 'the name the organization does business as')),
-                 ParserResource('customers', None,
-                                ('handle', 'the handle of the customer'),
-                                ('name', 'the name of the customer')),
-                 ParserResource('pocs', None,
-                                ('handle', 'the handle of the POC'),
-                                ('domain', 'the domain of the email address for the POC'),
-                                ('first', 'the first name of the POC'),
-                                ('last', 'the last name of the POC'),
-                                ('company', 'the company name registered by the POC'),
-                                ('city', 'the city registered by the POC')),
-                 ParserResource('asns', None,
-                                ('handle', 'the handle of the ASN'),
-                                ('name', 'the name of the ASN')),
-                 ParserResource('nets', None,
-                                ('handle', 'the handle of the network'),
-                                ('name', 'the name of the network')),
-                 ParserResource('rdns', None,
-                                ('name', 'the name of the delegation (e.g. 0.192.in-addr.arpa.)')),
-                 ParserResource('pocs-related', None,
-                                ('handle', 'the handle of the POC'),
-                                ('resource', 'the type of resource associated with the POC')))
+                 ParserResource('orgs',
+                                optional={'handle': 'the handle of the organization',
+                                          'name': 'the name of organization',
+                                          'dba': 'the name the organization does business as'}),
+                 ParserResource('customers',
+                                optional={'handle': 'the handle of the customer',
+                                          'name': 'the name of the customer'}),
+                 ParserResource('pocs',
+                                optional={'handle': 'the handle of the POC',
+                                          'domain': 'the domain of the email address for the POC',
+                                          'first': 'the first name of the POC',
+                                          'last': 'the last name of the POC',
+                                          'company': 'the company name registered by the POC',
+                                          'city': 'the city registered by the POC'}),
+                 ParserResource('asns',
+                                optional={'handle': 'the handle of the ASN',
+                                          'name': 'the name of the ASN'}),
+                 ParserResource('nets',
+                                optional={'handle': 'the handle of the network',
+                                          'name': 'the name of the network'}),
+                 ParserResource('rdns',
+                                optional={'name': 'the name of the delegation (e.g. 0.192.in-addr.arpa.)'}),
+                 ParserResource('pocs-related',
+                                optional={'handle': 'the handle of the POC',
+                                          'resource': 'the type of resource associated with the POC'}))
 
     def populate(self):
         self.add_argument('--output', choices=('xml', 'text', 'html'),
@@ -63,11 +65,10 @@ class Parser(argparse.ArgumentParser):
     def _add_subparser(self, subparsers, resource):
         subparser = subparsers.add_parser(resource.command,
                                           help=resource.help_)
-        if resource.positional:
-            subparser.add_argument(resource.positional[0],
-                                   metavar=resource.positional[0].upper(),
-                                   help=resource.positional[1])
-        for name, description in resource.option.iteritems():
+        for name, description in resource.positional.iteritems():
+            subparser.add_argument(name, metavar=name.upper(),
+                                   help=description)
+        for name, description in resource.optional.iteritems():
             subparser.add_argument('--%s' % name, metavar=name.upper(),
                                    help=description)
 
